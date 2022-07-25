25508541db358683b6439df3036575fb659bca61
phgm2 <109976463+phgm2@users.noreply.github.com>
2022-07-25 12:49:50 -0300

    Update index.php

--- example-PHP-jivo-api/index.php
+++ example-PHP-jivo-api/index.php
@@ -1,12 +1,12 @@
 <?php 
 	define('URL_API_JIVOCHAT',      'https://api.jivosite.com/web/integration');
 
-	$params['email'] = 'jonas339@gmail.com';
-	$params['siteUrl'] = 'www.exemple.com';
-	$params['userDisplayName'] =  'Antonio';
-	$params['userPassword'] =  'Senha12345';
-	$params['partnerId'] =  'Montink';
-	$params['partnerSecret'] =  'b_96XnsT!R';
+	$params['email'] = 'client_email_example@gmail.com'; //Your customer's email address that will be used to log in the JivoChat app
+	$params['siteUrl'] = 'www.example.com'; //The address of your client’s site. They could change it in the future. If a site with the same name already exists in the client’s account, you will receive a widget ID for this site, otherwise, a new one will be created.
+	$params['userDisplayName'] =  'Jhon Example Name'; //Customer’s real name to be displayed in chat. He will be able to change it in the future.
+	$params['userPassword'] =  'PasswordExample12345'; //Your customer's password for JivoChat, will be used to log in to JivoChat agent app. Must contain at least 6 characters, at least one number and a capital letter
+	$params['partnerId'] =  'YOURPARTNERID'; //We generate it for you
+	$params['partnerSecret'] =  'YOURPARTNERSECRET'; //We generate it for you
 	$params['authToken'] =  substr(str_shuffle('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'), 0, 30); //Here you can generate your token however you want
 	$params['lang'] = 'en';
 
